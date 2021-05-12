e3737e4be3ee717a7aea1f6b4603c70fdbd4e618
ANDRES R. BÜCHELI <radioseattle110@gmail.com>
2021-05-12 13:46:18 +0300

    using passport1

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+
 
 const app = express();
 
@@ -39,38 +39,11 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
 
-	bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-		const newUser = new User({
-			email: req.body.username,
-			password: hash
-		});
-		newUser.save(function(err){
-			if (err) {
-				console.log(err);
-			} else {
-				res.render("secrets")
-			}
-		});
-	});
+	
 });
     
 app.post("/login", function(req, res){
-	const username = req.body.username;
-	const password = req.body.password;
-
-	User.findOne({email: username}, function(err, foundUser) {
-		if(err) {
-			console.log(err);
-		} else {
-			if (foundUser) {
-				bcrypt.compare(password, foundUser.password, function(err, result) {
-    				if (result === true) {
-    					res.render("secrets");
-    				}
-				});
-			}
-		}
-	});
+	
 });
 
 app.listen(3000, function() {
