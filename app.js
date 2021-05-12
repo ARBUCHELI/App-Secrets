f6ea44831633a85a041a188ba3322289e0903b7f
ANDRES R. BÜCHELI <radioseattle110@gmail.com>
2021-05-12 15:21:34 +0300

    using passport2

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const app = express();
@@ -15,15 +17,30 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
+app.use(session({
+	secret: "Our little secret.",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
 	email: String,
 	password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
 	res.render("home");
