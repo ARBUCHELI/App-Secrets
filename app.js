9f4aebfb4aa244509aaf0684bb89ebc1a94158a5
ANDRES R. BÜCHELI <radioseattle110@gmail.com>
2021-05-12 10:30:11 +0300

    Using bcrypt3

--- app.js
+++ app.js
@@ -56,16 +56,18 @@ app.post("/register", function(req, res){
     
 app.post("/login", function(req, res){
 	const username = req.body.username;
-	const password = md5(req.body.password);
+	const password = req.body.password;
 
 	User.findOne({email: username}, function(err, foundUser) {
 		if(err) {
 			console.log(err);
 		} else {
 			if (foundUser) {
-				if (foundUser.password == password) {
-					res.render("secrets");
-				}
+				bcrypt.compare(password, foundUser.password, function(err, result) {
+    				if (result === true) {
+    					res.render("secrets");
+    				}
+				});
 			}
 		}
 	});
