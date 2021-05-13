031f6aa3af8c6e9e05ff4ddf82f647fbca9e8462
ANDRES R. BÜCHELI <radioseattle110@gmail.com>
2021-05-13 09:07:59 +0300

    using passport6

--- app.js
+++ app.js
@@ -80,7 +80,21 @@ app.post("/register", function(req, res){
 });
     
 app.post("/login", function(req, res){
-	
+
+	const user = new User({
+		username: req.body.username,
+		password: req.body.password
+	});
+
+	req.login(user, function(err){
+		if (err) {
+			console.log(err);
+		} else {
+			passport.authenticate("local")(req, res, function() {
+				res.redirect("/secrets");
+			});
+		}
+	});	
 });
 
 app.listen(3000, function() {
