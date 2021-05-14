b9a5c4f5c99706962cb8febc545ceada4e2f6220
ANDRES R. BÜCHELI <radioseattle110@gmail.com>
2021-05-14 08:18:44 +0300

    facebook auth1

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
@@ -66,6 +67,28 @@ passport.use(new GoogleStrategy({
     });
   }
 ));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({facebookId: profile.id }, function(err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/auth/facebook", passport.authenticate("facebook") 
+  );
+
+app.get("/auth/facebook/secrets",
+    passport.authenticate("facebook", { failureRedirect: "/login" }),
+    function(req, res) {
+		res.redirect("/secrets");
+    });
 
 app.get("/", function(req, res){
 	res.render("home");
